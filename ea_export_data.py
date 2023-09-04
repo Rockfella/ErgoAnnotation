0fba3129453c9a5b1101892e665adecc01ca769a
Rockfella <johansleman@gmail.com>
2023-09-04 15:17:13 +0200

    bugfix export

--- ea_export_data.py
+++ ea_export_data.py
@@ -6,6 +6,7 @@ import csv
 from .ea_constants import Constants
 from .ea_constants import frame_from_smpte
 
+
 def write_some_data(context, filepath, use_some_setting):
     print("running write_some_data...")
 
@@ -14,7 +15,6 @@ def write_some_data(context, filepath, use_some_setting):
     # Cache the sequence strips
     sequence_strips = list(scene.sequence_editor.sequences)
 
-    
     # MASTER TIME LIST
     data_master_clock = []
     # Projected frames from master clock
@@ -26,40 +26,45 @@ def write_some_data(context, filepath, use_some_setting):
     fps = bpy.context.scene.render.fps
     fps_base = bpy.context.scene.render.fps_base
     fps_real = fps / fps_base
-    
-    #Creating a list based of the master clock 
+
+    # Creating a list based of the master clock
     for frame in range(scene.frame_start, scene.frame_end + 1):
         # Input SMPTE formatted string
         smpte_string_current = bpy.utils.smpte_from_frame(
-        (frame + frames_from_master_clock - calc_master_frame), fps=fps, fps_base=fps_base)
+            (frame + frames_from_master_clock - calc_master_frame), fps=fps, fps_base=fps_base)
         data_master_clock.append(smpte_string_current)
-    
-    
 
     # Building a list of strips with their range and append them to the right dict
     sir_data_DUET_L = []
     sir_data_DUET_R = []
     sir_data_FREE_CHANNEL = []
-    for strip in sequence_strips:
+
+    # Sorting sequence_strips based on channel values in descending order
+    sorted_strips = sorted(sequence_strips, key=lambda s: s.channel, reverse=True)
+
+    for strip in sorted_strips:
         filter_name = strip.name.split(',')
         if strip.type == 'TEXT' and filter_name[0]:
             start_frame = int(strip.frame_start)
             end_frame = start_frame + int(strip.frame_final_duration)
-            filter_name = strip.name.split(',')
+            
 
-            #Sort the strip.names into the right cathegories 
+            filer_name_one_removed_spaces = filter_name[1].replace(
+                " ", "")
+
+            # Sort the strip.names into the right cathegories
             if filter_name[0] == Constants.DUET_LEFT[2]:
                 sir_data_DUET_L.append(
-                    (start_frame, end_frame, filter_name[1]))
+                    (start_frame, end_frame, filer_name_one_removed_spaces))
 
             elif filter_name[0] == Constants.DUET_RIGHT[2]:
-               sir_data_DUET_R.append((start_frame, end_frame, filter_name[1]))
-                   
+                sir_data_DUET_R.append(
+                    (start_frame, end_frame, filer_name_one_removed_spaces))
+
             elif filter_name[0] == Constants.FREE_CHANNEL[2]:
-               sir_data_FREE_CHANNEL.append(
-                   (start_frame, end_frame, filter_name[1]))
-                   
-            
+                sir_data_FREE_CHANNEL.append(
+                    (start_frame, end_frame, filer_name_one_removed_spaces))
+
     # Sort the list by start_frame
     sir_data_DUET_L.sort()
     sir_data_DUET_R.sort()
@@ -71,7 +76,7 @@ def write_some_data(context, filepath, use_some_setting):
     active_strips_data_FREE_CHANNEL = {}
     for frame in range(scene.frame_start, scene.frame_end + 1):
         active_strips_data_DUET_L[frame] = [name for start, end,
-                                name in sir_data_DUET_L if start <= frame < end]
+                                            name in sir_data_DUET_L if start <= frame < end]
         active_strips_data_DUET_R[frame] = [name for start, end,
                                             name in sir_data_DUET_R if start <= frame < end]
         active_strips_data_FREE_CHANNEL[frame] = [name for start, end,
@@ -81,16 +86,16 @@ def write_some_data(context, filepath, use_some_setting):
     data_data_DUET_R = []
     data_FREE_CHANNEL = []
 
-    for frame, strips in active_strips_data_DUET_L.items(): 
+    for frame, strips in active_strips_data_DUET_L.items():
         data_data_DUET_L.append(strips)
-    
+
     for frame, strips in active_strips_data_DUET_R.items():
         data_data_DUET_R.append(strips)
-    
+
     for frame, strips in active_strips_data_FREE_CHANNEL.items():
         data_FREE_CHANNEL.append(strips)
 
-    #Combine all lists into a dict that speeds up the csv export
+    # Combine all lists into a dict that speeds up the csv export
     data = {
         "master_clock": data_master_clock,
         "DUET_L": data_data_DUET_L,
@@ -98,8 +103,7 @@ def write_some_data(context, filepath, use_some_setting):
         "FREE_CHANNEL": data_FREE_CHANNEL
     }
 
-
-    #Save to csv file
+    # Save to csv file
     with open(filepath, 'w', encoding='utf-8', newline='') as f:
         writer = csv.DictWriter(f, fieldnames=data.keys())
         writer.writeheader()
@@ -148,23 +152,23 @@ class ExportSomeData(Operator, ExportHelper):
         return write_some_data(context, self.filepath, self.use_setting)
 
 
-## Only needed if you want to add into a dynamic menu
-#def menu_func_export(self, context):
+# Only needed if you want to add into a dynamic menu
+# def menu_func_export(self, context):
 #    self.layout.operator(ExportSomeData.bl_idname, text="Text Export Operator")
 #
 #
-## Register and add to the "file selector" menu (required to use F3 search "Text Export Operator" for quick access).
-#def register():
+# Register and add to the "file selector" menu (required to use F3 search "Text Export Operator" for quick access).
+# def register():
 #    bpy.utils.register_class(ExportSomeData)
 #    bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
 #
 #
-#def unregister():
+# def unregister():
 #    bpy.utils.unregister_class(ExportSomeData)
 #    bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
 #
 #
-#if __name__ == "__main__":
+# if __name__ == "__main__":
 #    register()
 #
 #    # test call
